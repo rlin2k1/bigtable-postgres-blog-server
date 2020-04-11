cf5b3904d0b975ef7664de576391b4c4672fff2c
Roy Sanga Lin <rlin2k1@g.ucla.edu>
2020-04-11 20:26:05 +0000

    What: Includes a HTTP Request parser to determine valid or invalid incoming HTTP Requests. Why: Sometimes users can send bad formatted HTTP Requests and the Server does not know how to handle them. How: Added a Request Parser from the BOOST example library to the codebase and integrated the request parser to determine good/bad formatted requests.
    Change-Id: Idf12a77c4789778f481271cd94e84fb155ac8f7e

--- include/session.h
+++ include/session.h
@@ -1,5 +1,8 @@
 #include <boost/asio.hpp>
 
+#include "request.h"
+#include "request_parser.h"
+
 class session {
 	public:
 		session(boost::asio::io_service& io_service);
@@ -9,7 +12,13 @@ class session {
 	private:
 		void handle_read(const boost::system::error_code& error, size_t bytes_transferred);
 		void handle_write(const boost::system::error_code& error);
+		void shutdown(const boost::system::error_code& error);
 		boost::asio::ip::tcp::socket socket_;
 		enum { max_length = 1024 };
 		char data_[max_length];
+
+		// The incoming request.
+  		http::server::request request_;
+  		// The parser for the incoming request.
+  		http::server::request_parser request_parser_;
 };
--- src/session.cc
+++ src/session.cc
@@ -24,14 +24,35 @@ void session::start() {
 void session::handle_read(const boost::system::error_code& error, size_t bytes_transferred) {
 	if (!error) {
 		// Asynchronously writes to the socket_ everything in the buffer. 
-		std::string resp = "HTTP/1.1 200 OK/\r\nContent-Length: " + std::to_string(bytes_transferred) + "\r\nContent-Type: text/plain\r\n\r\n";
+		http::server::request_parser::result_type result;
+		std::tie(result, std::ignore) = request_parser_.parse(
+              request_, data_, data_ + bytes_transferred);
 
-		int buffer_length = resp.length() + bytes_transferred;
+		if (result == http::server::request_parser::good) {
+			std::string resp = "HTTP/1.1 200 OK/\r\nContent-Length: " + std::to_string(bytes_transferred) + "\r\nContent-Type: text/plain\r\n\r\n";
 
-		boost::asio::async_write(socket_,
-			boost::asio::buffer(resp + data_, buffer_length),
-			boost::bind(&session::handle_write, this,
-			boost::asio::placeholders::error));
+			int buffer_length = resp.length() + bytes_transferred;
+
+            boost::asio::async_write(socket_,
+				boost::asio::buffer(resp + data_, buffer_length),
+				boost::bind(&session::handle_write, this,
+				boost::asio::placeholders::error));
+        } else if (result == http::server::request_parser::bad) {
+			std::string resp = "HTTP/1.1 400 Bad Request\r\n";
+
+			int buffer_length = resp.length();
+
+            boost::asio::async_write(socket_,
+				boost::asio::buffer(resp, buffer_length),
+				boost::bind(&session::shutdown, this,
+				boost::asio::placeholders::error));
+        } else { // Keep on Reading
+            socket_.async_read_some(boost::asio::buffer(data_, max_length), 
+				boost::bind(&session::handle_read, this,
+				boost::asio::placeholders::error,
+				boost::asio::placeholders::bytes_transferred));
+        }
+		// After the write operation completes, we call boosts::binds() -> The Write Function
 	} else {
 		delete this;
 	}
@@ -43,6 +64,16 @@ void session::handle_write(const boost::system::error_code& error) {
 			boost::bind(&session::handle_read, this,
 			boost::asio::placeholders::error,
 			boost::asio::placeholders::bytes_transferred));
+	} else {
+		delete this;
+	}
+}
+
+void session::shutdown(const boost::system::error_code& error) {
+	if (!error) {
+		boost::system::error_code ignored_ec;
+          	socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both,
+            ignored_ec);
 	} else {
 		delete this;
 	}
