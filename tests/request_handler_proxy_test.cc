ae882e71e36989fa8bc35bb4836503a1be349e7b
dahn <dahn@g.ucla.edu>
2020-05-24 19:01:51 -0700

    WHAT: Unit tests for request_handler_proxy.cc
    TESTING: Fetches image from our server radd.cs130.org and the ucla website www.ucla.edu
    Change-Id: I59a954a9f7455d8a971f886b9b33c4e58dcc39af

---
+++ tests/request_handler_proxy_test.cc
@@ -0,0 +1,58 @@
+#include "gtest/gtest.h"
+#include "proxy_request_handler.h"
+#include "config_parser.h"
+#include "request.h"
+
+class Proxy_Request_HandlerTest : public ::testing::Test {
+ protected:
+  std::unique_ptr<proxy_request_handler> proxy_request_handler_;
+  Request request_;
+  NginxConfig config;
+  void SetUp() override {
+    NginxConfigParser parser;
+    parser.Parse("proxy_test_config", &config);
+  }
+
+  void SetHandler(std::string path, std::string uri) {
+    proxy_request_handler_.reset(proxy_request_handler::Init(path, config));
+    request_.method_ = Request::MethodEnum::GET;
+    request_.uri_ = path + uri;
+    request_.version_ = "HTTP/1.1";
+  }
+
+  std::string GetGolden(std::string path) {
+    std::ifstream fs(path, std::ios_base::in);
+    std::stringstream ss;
+    ss << fs.rdbuf();
+    return ss.str();
+  }
+
+  void RemovePath(std::string& body, std::string& path) {
+    std::string::size_type n = path.length();
+    for (std::string::size_type i = body.find(path); i != std::string::npos;
+         i = body.find(path)) {
+      body.erase(i, n);
+    }
+  }
+};
+
+TEST_F(Proxy_Request_HandlerTest, SimpleTest) {
+  // Tests if proxy handler properly gets hotdog image from radd.cs130.org
+  std::string golden = GetGolden("./hotdoggolden.jpeg");
+  std::string path = "/simpletest";
+  SetHandler(path, "/static/images/hotdog.jpeg");
+  Response test = proxy_request_handler_->handle_request(request_);
+  EXPECT_EQ(test.code_, 200);
+  EXPECT_EQ(test.body_, golden);
+}
+
+TEST_F(Proxy_Request_HandlerTest, ExampleTest) {
+  // Tests if proxy handler properly gets website wwww.example.com
+  std::string golden = GetGolden("./example.html");
+  std::string path = "/exampletest";
+  SetHandler(path, "");
+  Response test = proxy_request_handler_->handle_request(request_);
+  RemovePath(test.body_, path);
+  EXPECT_EQ(test.code_, 200);
+  EXPECT_EQ(test.body_, golden);
+}
